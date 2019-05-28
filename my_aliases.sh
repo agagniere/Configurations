f97bcc5fa1eb8d43e5d7d2b86cf0e7e39b8df362
Antoine Gagniere <angagnie@student.42.fr>
2019-05-28 09:49:51 +0200

    General aliases in my_aliases.sh

---
+++ my_aliases.sh
@@ -0,0 +1,17 @@
+alias emacs="emacs -nw"
+alias e=emacs
+
+alias ls="ls --color"
+alias ll="ls -l"
+alias la="ls -la"
+
+alias gits="git status"
+alias gitd="git diff"
+alias gitdc="git diff --cached"
+alias gitl="git log --oneline"
+
+alias wai="clear ; echo ; pwd ; echo ; ls"
+alias src="source ~/.zshrc"
+
+alias clean="find . \( -name '*~' -o -name '#*#' -o -name '.swp' \) -print -delete"
+alias cleanpy="find . -name '*.pyc' -print -delete"

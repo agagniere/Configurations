e795197f94b679a14005b39c637729554f4a66b5
Antoine Gagniere <angagnie@student.42.fr>
2019-05-28 09:46:17 +0200

    Divide shell configuration into several files

--- 42_aliases.sh
+++ 42_aliases.sh
@@ -1,45 +1,18 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    .zshrc                                             :+:      :+:    :+:    #
+#    42_aliases.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/11 11:34:22 by angagnie          #+#    #+#              #
-#    Updated: 2017/03/06 18:20:44 by angagnie         ###   ########.fr        #
+#    Updated: 2019/05/28 09:42:27 by angagnie         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-PATH=$HOME/.brew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin
-HISTFILE=~/.zshrc_history
-SAVEHIST=10000
-HISTSIZE=10000
-
-setopt inc_append_history
-setopt share_history
-
-if [[ -f ~/.myzshrc ]]; then
-  source ~/.myzshrc
-fi
-
-export HISTCONTROL=erasedups
-
-USER=`/usr/bin/whoami`
-export USER
-GROUP=`/usr/bin/id -gn $user`
-export GROUP
-MAIL="$USER@student.42.fr"
-export MAIL
-
-# My personnal aliases
-alias wai="clear ; echo -e '\n\t\t--===\033[0;31m Hello Antoine \033[1;0m===--\n' ; pwd ; ls -G"
-alias clean="find . \( -name '*~' -o -name '#*#' -o -name '*.DS_Store' -o -name 'a.out' -o -name '*.swp' -o -name '*.h.gch' \) -print -delete"
-alias waigit="clear ; echo -e '\n\t\t--=== Hello ===--\n' ; git status ; pwd ; ls -G"
 alias gccf="gcc -Wall -Wextra -Werror "
 alias cacherm="rm -rf ~/Library/Caches/com.spotify.client/Storage/ ; rm -rf ~/.caches ; rm -rf ~/.valgrind ; rm -rf ~/.*~ ; rm -rf ~/.serverauth* ; rm -rf ~/.Trash/* ; rm -rf ~/.bash_history ; rm -rf ~/Library/Caches/Google ; rm -rf ~/Library/Caches/*Update* ; rm -rf `brew --cache` ; brew cleanup -s --force ; rm -rf ~/Library/Caches/com.apple.helpd ; rm -rf ~/Library/Developer"
-alias src="source ~/.zshrc"
 alias norm="norminette **/*.c **/*.h | grep -v Norme -B1"
 alias nvcc=/Developer/NVIDIA/CUDA-7.0/bin/nvcc
 alias vim=emacs
 alias vi=emacs
-alias e=emacs

c9b62331836afaac9563dc1b8b0888aef01320aa
Antoine <angagnie@student.42.fr>
2022-10-28 16:48:04 +0200

    Add pyenv for macos

--- macos/environment.sh
+++ macos/environment.sh
@@ -1,5 +1,11 @@
 # Set PATH, MANPATH, etc., for Homebrew.
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Autocomplete
 FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
 autoload -Uz compinit
 compinit
+
+# Pyenv
+PYENV_ROOT="~/.pyenv"
+eval "$(pyenv init -)"

5a7ee7e85f5b0214fe423a989f2657082cfae83f
Antoine Gagniere <antoine@gagniere.dev>
2025-10-07 20:37:39 +0200

    List a few CLI programs that can easily be compiled and installed using Rust & Go

---
+++ install_clis.sh
@@ -0,0 +1,10 @@
+cargo install --git https://github.com/typst/typst.git --tag v0.13.1 typst-cli # --locked
+cargo install --git https://github.com/astral-sh/uv.git --tag 0.8.22 uv # --locked
+
+go install github.com/mikefarah/yq/v4@latest
+go install github.com/tomwright/dasel/v3/cmd/dasel@master
+go install github.com/charmbracelet/glow/v2@latest
+go install github.com/google/go-jsonnet/cmd/jsonnet@latest
+go install github.com/google/go-jsonnet/cmd/jsonnet-lint@latest
+go install github.com/google/go-jsonnet/cmd/jsonnetfmt@latest
+go install github.com/google/go-jsonnet/cmd/jsonnet-deps@latest

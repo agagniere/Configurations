811ea5f1c848639fa69da24abf888d2f4c85aa8a
Antoine GAGNIERE <antoine.gagniere@datadoghq.com>
2026-01-22 15:56:18 +0100

    clis: Add goroutine-inspect, add assume-yes option

--- install_clis.sh
+++ install_clis.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+assume=$1
+
 function cargo_install_latest()
 {
 	local site=$1
@@ -8,10 +10,10 @@ function cargo_install_latest()
 	local regex=${4:-'v?\d+[.]\d+[.]\d+$'}
 
 	local url="https://$site/$name"
-	local tag=$(git ls-remote --tags $url | grep -P $regex --only-matching | sort -V | tail -1)
+	local tag=$(git ls-remote --tags $url | grep -E $regex --only-matching | sort -V | tail -1)
 	local command="cargo install --git $url --tag $tag $binary --locked"
 
-	if $(gum confirm "Install $name version $tag ?")
+	if test -z "$assume" && $(gum confirm "Install $name version $tag ?") || test "$assume" = "-y"
 	then
 		gum spin --spinner dot --title "[cargo] Installing $name version $tag" -- $command
 		gum log --time TimeOnly --structured --level info "$name" version $tag
@@ -24,9 +26,10 @@ function go_install_latest()
 {
 	local module=$1
 	local name=${2:-$(basename $module)}
-	local command="go install ${module}@latest"
+	local version=${4:-"latest"}
+	local command="go install ${module}@${version}"
 
-	gum spin --spinner dot --title "[go] Installing $name" --show-output -- $command
+	gum spin --spinner dot --title "[go] Installing $name $version" --show-output -- $command
 	gum log --time TimeOnly --level info $name
 }
 
@@ -37,7 +40,7 @@ cargo_install_latest github.com weezy20/zv
 cargo_install_latest github.com nushell/nushell nu '\d+[.]\d+[.]\d+$'
 cargo_install_latest github.com astral-sh/uv uv
 cargo_install_latest github.com bytecodealliance/wasmtime wasmtime-cli
-cargo_install_latest github.com bytecodealliance/wasm-tools
+cargo_install_latest github.com bytecodealliance/wasm-tools wasm-tools
 cargo_install_latest github.com bytecodealliance/wit-bindgen
 cargo_install_latest github.com typstyle-rs/typstyle
 
@@ -50,3 +53,4 @@ go_install_latest github.com/google/go-jsonnet/cmd/jsonnetfmt
 go_install_latest github.com/google/go-jsonnet/cmd/jsonnet-deps
 go_install_latest github.com/go-delve/delve/cmd/dlv delve
 go_install_latest github.com/krzko/otelgen/cmd/otelgen
+go_install_latest github.com/linuxerwang/goroutine-inspect goroutine-inspect master
